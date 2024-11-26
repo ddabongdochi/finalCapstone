fa7bdd1f716c73860ad3037be8c5222845996f74
ddabongdochi <hooyhoon5019@gmail.com>
2024-11-27 00:37:31 +0900

    community

--- community/views.py
+++ community/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from login.models import UserProfile
+
+
 # Create your views here.
 
 @login_required
 def community(request):
-    return render(request, 'community/community.html')
+    stock_symbol = request.GET.get('symbol', 'Unknown')
+    stock_name = request.GET.get('name', 'Unknown Stock')
+    return render(request, 'community/community.html', {
+        'stock_symbol': stock_symbol,
+        'stock_name': stock_name,
+    })
